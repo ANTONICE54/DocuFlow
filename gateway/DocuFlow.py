c88dc61126036d77dd671424be3f5737ce975ac0
Dimaengineer <dmitrokds@gmail.com>
2024-10-14 17:13:23 +0300

    modified:   gateway/DocuFlow.py

--- gateway/DocuFlow.py
+++ gateway/DocuFlow.py
@@ -39,6 +39,9 @@ class add_result(BaseModel):
 class create_result(BaseModel):
     create_result: str
 
+class create_tags(BaseModel):
+    tags: list[str]
+
 
 @docu_flow_router.post("/extract_data_from_file/")
 async def extract_data_from_file(input_file: UploadFile = File(...)) -> extract_data_from_file_result:
@@ -54,9 +57,12 @@ async def extract_data_from_file(input_file: UploadFile = File(...)) -> extract_
 
 
 
-@docu_flow_router.get("/get_explanation")
-async def get_explanation(document_text: str) -> get_explanation_result:
+@docu_flow_router.post("/get_explanation")
+async def get_explanation(document_text: UploadFile) -> get_explanation_result:
+    if document_text.content_type != "text/plain":
+        raise HTTPException(400, detail="document_text must be .txt")
     try:
+        document_text = document_text.file.read().decode('utf-8')
         chat_gpt_request = openai.OpenAI(api_key=CHAT_GPT_TOKEN)
 
         explanation_result = chat_gpt_request.chat.completions.create(
@@ -73,9 +79,14 @@ async def get_explanation(document_text: str) -> get_explanation_result:
 
 
 
-@docu_flow_router.get("/get_explanation_with_request")
-async def get_explanation_with_request(document_text: str, explain_request: str) -> get_explanation_result:
+@docu_flow_router.post("/get_explanation_with_request")
+async def get_explanation_with_request(document_text: UploadFile, explain_request: str) -> get_explanation_result:
+    if document_text.content_type != "text/plain":
+        raise HTTPException(400, detail="document_text must be .txt")
+
     try:
+        document_text = document_text.file.read().decode('utf-8')
+
         chat_gpt_request = openai.OpenAI(api_key=CHAT_GPT_TOKEN)
 
         explanation_result = chat_gpt_request.chat.completions.create(
@@ -93,10 +104,16 @@ async def get_explanation_with_request(document_text: str, explain_request: str)
 
 
 
-@docu_flow_router.get("/get_part_explanation")
-async def get_part_explanation(document_text: str, explain_part: str="") -> get_explanation_result:
-    print(explain_part)
+@docu_flow_router.post("/get_part_explanation")
+async def get_part_explanation(document_text: UploadFile, explain_part: UploadFile) -> get_explanation_result:
+    if document_text.content_type != "text/plain":
+        raise HTTPException(400, detail="document_text must be .txt")
+    elif explain_part.content_type != "text/plain":
+        raise HTTPException(400, detail="explain_part must be .txt")
+
     try:
+        document_text = document_text.file.read().decode('utf-8')
+        explain_part = explain_part.file.read().decode('utf-8')
         chat_gpt_request = openai.OpenAI(api_key=CHAT_GPT_TOKEN)
 
         explanation_result = chat_gpt_request.chat.completions.create(
@@ -104,7 +121,7 @@ async def get_part_explanation(document_text: str, explain_part: str="") -> get_
             messages=[
                 {"role": "system", "content": f'document:{document_text}'},
                 {"role": "system", "content": f'explain part:{explain_part}'},
-                {"role": "assistant", "content": f'(Country Ukraine).\nMake very easy explanation of this document by user explanaition request in super simple words for an ordinary person, but do not forget about the details (names, numbers, etc.). As a result, you will receive a text about this document in the language of the country. Finally, make it look good. Make more than 400 characters'}
+                {"role": "assistant", "content": f'(Country Ukraine).\nMake very easy explanation of this document by user explanaition request in super simple words for an ordinary person, but do not forget about the details (names, numbers, etc.). As a result, you will receive a text about this document in the language of the country. Finally, make it look good. Make more than 500 characters'}
             ]
         ).choices[0].message.content
     except Exception as explanation_error:
@@ -114,9 +131,17 @@ async def get_part_explanation(document_text: str, explain_part: str="") -> get_
 
 
 
-@docu_flow_router.get("/replace_document_part")
-async def replace_document_part(document_text: str, replace_document_part: str, replace_request: str = "") -> replace_result:
+@docu_flow_router.post("/replace_document_part")
+async def replace_document_part(document_text: UploadFile, replace_document_part: UploadFile, replace_request: str = "") -> replace_result:
+    if document_text.content_type != "text/plain":
+        raise HTTPException(400, detail="document_text must be .txt")
+    elif replace_document_part.content_type != "text/plain":
+        raise HTTPException(400, detail="replace_document_part must be .txt")
+
     try:
+        document_text = document_text.file.read().decode('utf-8')
+        replace_document_part = replace_document_part.file.read().decode('utf-8')
+
         chat_gpt_request = openai.OpenAI(api_key=CHAT_GPT_TOKEN)
 
         replace_result = chat_gpt_request.chat.completions.create(
@@ -135,9 +160,14 @@ async def replace_document_part(document_text: str, replace_document_part: str, 
 
 
 
-@docu_flow_router.get("/add_document_part")
-async def replace_document_part(document_text: str, add_request: str) -> add_result:
+@docu_flow_router.post("/add_document_part")
+async def replace_document_part(document_text: UploadFile, add_request: str) -> add_result:
+    if document_text.content_type != "text/plain":
+        raise HTTPException(400, detail="document_text must be .txt")
+
     try:
+        document_text = document_text.file.read().decode('utf-8')
+
         chat_gpt_request = openai.OpenAI(api_key=CHAT_GPT_TOKEN)
 
         add_result = chat_gpt_request.chat.completions.create(
@@ -155,9 +185,14 @@ async def replace_document_part(document_text: str, add_request: str) -> add_res
 
 
 
-@docu_flow_router.get("/create_document_by_template")
-async def create_document_by_template(template_document_text: str, create_request: str) -> create_result:
+@docu_flow_router.post("/create_document_by_template")
+async def create_document_by_template(template_document_text: UploadFile, create_request: str) -> create_result:
+    if template_document_text.content_type != "text/plain":
+        raise HTTPException(400, detail="template_document_text must be .txt")
+
     try:
+        template_document_text = template_document_text.file.read().decode('utf-8')
+
         chat_gpt_request = openai.OpenAI(api_key=CHAT_GPT_TOKEN)
 
         create_result = chat_gpt_request.chat.completions.create(
@@ -172,6 +207,31 @@ async def create_document_by_template(template_document_text: str, create_reques
         raise HTTPException(400, detail=f"Create was failed\n\n{create_error}")
 
     return {"create_result": create_result}
+
+
+
+@docu_flow_router.post("/create_tags")
+async def create_tags_function(document_text: UploadFile) -> create_tags:
+    if document_text.content_type != "text/plain":
+        raise HTTPException(400, detail="document_text must be .txt")
+
+    try:
+        document_text = document_text.file.read().decode('utf-8')
+
+        chat_gpt_request = openai.OpenAI(api_key=CHAT_GPT_TOKEN)
+
+        create_result = eval(chat_gpt_request.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": f'document:{document_text}'},
+                {"role": "assistant", "content": f'(Country Ukraine) Read the document. Add 10 tags that are most relevant to this document in country language. With this tag you can easy describe document. Do it in ["tag", "tag", "tag"] format because I can turn it into a list'}
+            ]
+        ).choices[0].message.content)
+        print(create_result)
+    except Exception as create_error:
+        raise HTTPException(400, detail=f"Create was failed\n\n{create_error}")
+
+    return {"tags": create_result}
 
 
 
