7dae8ffd0e9cd6968852e060135155c4b3ba529e
Antonice <oliynik262004@gmail.com>
2024-10-15 21:48:30 +0300

    Added dockerfiles

--- gateway/sign_check.py
+++ gateway/sign_check.py
@@ -1,3 +1,5 @@
+import requests
+
 def check_sign_in(sign_data):
     for data_name, data_value in sign_data.items():
         if data_value=='':
@@ -13,6 +15,16 @@ def check_sign_up(sign_data):
         
     if sign_data["password"] != sign_data["password_confirmation"]:
         return f"{data_name} пітвердження паролю не співпадає"
-        
+    data = {
+        "name": sign_data["name"],
+        "surname": sign_data["surname"],
+        "email": sign_data["email"],
+        "country": sign_data["country"],
+        "password":sign_data["password"]
+    }
+
+    #TODO -- check status code
+    requests.post("http://user_service:8080/register", json=data)
+
     return "succes"
     
