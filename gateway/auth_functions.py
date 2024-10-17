f133a692ba1308d57ed121d99fd5eb383ada607c
Dimaengineer <dmitrokds@gmail.com>
2024-10-17 19:53:57 +0300

    modified:   gateway/auth_functions.py

--- gateway/auth_functions.py
+++ gateway/auth_functions.py
@@ -1,7 +1,14 @@
 import requests
 
-def register_profile(name, surname, email, country, password, password_confirmation):
-    pass
+def register_profile(name, surname, email, country, password):
+    register_status = 0
+
+    for register_attempt in range(3):
+        try:
+            response = requests.get(url, params= params, timeout=20)
+        except:
+            pass
+        register_status = response.status_code
 
 def log_in_profile(email, password):
     pass
