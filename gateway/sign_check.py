2ae646fcaea7c0fb8c477bbc8623c7341c750528
DmitroKDS <74712259+DmitroKDS@users.noreply.github.com>
2024-10-17 21:07:12 +0300

    Update sign_check.py

--- gateway/sign_check.py
+++ gateway/sign_check.py
@@ -1,3 +1,5 @@
+import requests
+
 def check_sign_in(sign_data):
     for data_name, data_value in sign_data.items():
         if data_value=='':
@@ -12,7 +14,14 @@ def check_sign_up(sign_data):
             return f"{data_name} є порожній"
         
     if sign_data["password"] != sign_data["password_confirmation"]:
-        return f"Пітвердження паролю не співпадає"
-        
+        return f"{data_name} пітвердження паролю не співпадає"
+    data = {
+        "name": sign_data["name"],
+        "surname": sign_data["surname"],
+        "email": sign_data["email"],
+        "country": sign_data["country"],
+        "password":sign_data["password"]
+    }
+
     return "succes"
     
